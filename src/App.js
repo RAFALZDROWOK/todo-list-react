4973579e91911aa0ba8f093c1a7c270df25d8112
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-06-19 11:07:31 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -24,20 +24,31 @@ function App() {
   };
 
   const toggleTaskDone = (id) => {
-      setTasks(tasks => tasks.map(task => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
 
-        return task;
-      }));
+      return task;
+    }));
   };
 
   const setAllDone = () => {
-    setTasks(task => tasks.map(task => ({
+    setTasks(tasks => tasks.map(task => ({
       ...task,
       done: true,
     })));
+  };
+
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
   };
 
   return (
@@ -45,18 +56,18 @@ function App() {
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
 
       <Section
         title="Lista zadań"
         body={
-          <Tasks 
-          tasks={tasks} 
-          hideDone={hideDone} 
-          removeTask={removeTask}
-          toggleTaskDone={toggleTaskDone}
-           />}
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />}
         extraHeaderContent={
           <Buttons
             tasks={tasks}
