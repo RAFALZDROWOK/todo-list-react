824dd3c30f5fe10b7c67aacbb85310d0e927b69f
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-09-26 12:08:24 +0200

    added styled-components to Buttons component

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -1,27 +1,28 @@
-.buttons {
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
-}
+`;
 
-.buttons__button {
+export const Button = styled.button`
     background: transparent;
     color: teal;
     border: none;
     margin: 0 0 0 20px;
     transition: color 0.3s;
-}
 
-@media(max-width: 767px){
-    .buttons__button {
+    @media(max-width: 767px){
         flex-basis: 100%;
         margin: 10px;
     }
-}
 
-.buttons__button:hover {
+    &:hover {
     color: hsl(180, 100%, 35%);
-}
-
-.buttons__button:disabled {
+    }
+    &:disabled {
     color: rgb(232, 230, 230);
-}
+    }
+`;
+
+
