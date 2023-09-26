2249276beacbe7fd2cffa31669212a4c1435a267
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-09-26 12:07:08 +0200

    added styled-components to Buttons component and deleted file css

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,17 +1,18 @@
-import "./style.css";
+import { Wrapper } from "./styled";
+import { Button } from "./styled";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     tasks.length > 0 && (
-        <div className="buttons">
-            <button onClick={toggleHideDone} className="buttons__button">
+        <Wrapper>
+            <Button onClick={toggleHideDone}>
                 {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button onClick={setAllDone} className="buttons__button"
+            </Button>
+            <Button onClick={setAllDone} 
                 disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
-            </button>
-        </div>
+            </Button>
+        </Wrapper>
     ));
 
 export default Buttons;
