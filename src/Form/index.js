94ad824b149237f35aa0e2f24d4a2ffcf11502ed
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-09-28 12:28:02 +0200

    a useRef Hook has been introduced that allows you to set focus to input between renders

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     if (newTaskContent.trim() !== "")
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    inputRef.current.focus();
   };
 
   return (
     <StyledForm onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         type="text"
         placeholder="Co jest do zrobienia?"
